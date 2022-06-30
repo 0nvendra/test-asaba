84240433477f05cf083456277a1bda3ac19f5d89
0nvendra <onvendra@gmail.com>
2022-06-30 16:15:02 +0800

    first commit

---
+++ app/Http/Controllers/PostageController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Postage;
+use Illuminate\Http\Request;
+
+class PostageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Postage  $postage
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Postage $postage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Postage  $postage
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Postage $postage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Postage  $postage
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Postage $postage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Postage  $postage
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Postage $postage)
+    {
+        //
+    }
+}
